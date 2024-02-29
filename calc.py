46e0a962ef81977fab5e7e69a74945f5dd6a0ecc
TheOne_Neo <nemohap022@pop-os.localdomain>
2024-02-29 09:59:41 +0200

    base

---
+++ calc.py
@@ -0,0 +1,28 @@
+def addition(a , b):
+    pass
+
+
+def subtraction(a , b):
+    pass
+
+
+def multiplication(a , b):
+    pass
+
+
+
+def division(a , b):
+    pass
+
+
+def grouping(a , b):
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    pass
